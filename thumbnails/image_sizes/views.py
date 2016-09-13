c5933a8ec4183df63035971656dee5753b127fee
BK Bolisetty <bk@squarefruitlabs.com>
2016-09-13 15:45:24 -0700

    Save this checkpoint for upload.
    Upload doesn't work yet. May be in the future.

--- thumbnails/image_sizes/views.py
+++ thumbnails/image_sizes/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.template.defaulttags import csrf_token
+from django.shortcuts import render
+from django import forms
 import re
 import os
 
@@ -8,27 +11,54 @@ CURR_PATH = os.path.abspath('./')
 all_sizes = []
 all_count = []
 
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+
 def image_size(s):
     out = re.findall('\d+x\d+', s)
     return out[-1]
 
-with open('image_sizes/fun-rooster-thumbnail-names.txt') as test:
-    content = test.readlines()
-    for ln in content:
-        matches = image_size(ln)
-        if matches in all_sizes:
-            all_count[all_sizes.index(matches)] += 1
-        else:
-            all_sizes.append(matches)
-            all_count.append(1)
+filename = 'image_sizes/fun-rooster-thumbnail-names.txt'
+
+def fun_imgsz(fn):
+    with open(fn) as test:
+        content = test.readlines()
+        for ln in content:
+            matches = image_size(ln)
+            if matches in all_sizes:
+                all_count[all_sizes.index(matches)] += 1
+            else:
+                all_sizes.append(matches)
+                all_count.append(1)
 
 def index(request):
-    #sizes = all_sizes
-    #count = all_count
+    total = sum(all_count)
     lst = zip(all_sizes, all_count)
-    template = loader.get_template(CURR_PATH + '/image_sizes/templates/image_sizes/index.html')
+    template = loader.get_template(os.path.join(CURR_PATH, 'image_sizes/templates/image_sizes/index.html'))
     context = {
         'lst': lst,
+        'total': total,
+        'filename' : filename,
     }
     return HttpResponse(template.render(context, request))
+
+fun_imgsz(filename)
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            write_file(request.FILES['file'])
+            return HttpResponseRedirect('index.html')
+    else:
+        form = UploadFileForm()
+        return render(request, 'index.html', {'form': form})
+def myview(request):
+    request.FILES['myfile']
+
+def write_file(f):
+    with open(MEDIA_ROOT, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
